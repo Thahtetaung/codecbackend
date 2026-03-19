d2c8c3b1aaa904e27d173a24374763340f488fa0
Thar <thahtetaung123@gmail.com>
2026-03-19 03:27:08 -0500

    add route

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="flex flex-col justify-between px-12 py-2">
     <div class="border-2">
         <div>
-            <a href="#">Admin</a>
+            <a href="/">Admin</a>
         </div>
         <div class="flex flex-col">
-            <a href="#">Dashboard</a>
-            <a href="#">Projects</a>
+            <a href="/">Dashboard</a>
+            <a href="/project/create">Projects</a>
             <a href="#">Teammember</a>
         </div>
     </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,2 +1,5 @@
 <x-layout>
+
+    <h1>Here will come the homepage</h1>
+    
 </x-layout>
